2ea17c5d0166851a9a7cace408f50eafae9501a5
aznoqmous <paul.landreau931v@gmail.com>
2025-11-21 12:34:01 +0100

    tonguableFoliage

--- scripts/character_base.gd
+++ scripts/character_base.gd
@@ -192,7 +192,9 @@ func _on_tongue_area_3d_body_entered(body: RigidBody3D) -> void:
 	tongue_area_3d.monitoring = false
 	tongue_tween.stop()
 	tongue_tween.finished.emit()
-	body.apply_central_impulse((global_position - body.global_position).normalized() * 10.0)
+	if body is TonguableFoliage and self == main.player:
+		if body.tonguable and body.tonguable_level <= main.player.species[SpeciesResource.ActionType.Tongue].current_level:
+			body.apply_central_impulse((global_position - body.global_position).normalized() * 10.0)
 
 signal jumped()
 signal charged()
