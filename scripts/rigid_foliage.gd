2f4df8d464723289c0dc598c53349e6847f4bb2a
Khalerzaad <khalerzaad@gmail.com>
2025-11-21 11:43:13 +0100

    Ram level update with wall + rigid foliage two sprite update

--- scripts/rigid_foliage.gd
+++ scripts/rigid_foliage.gd
@@ -3,6 +3,14 @@ class_name RigidFoliage extends Foliage
 
 @export var charge_breakable := false
 @export var charge_breakable_level := 0
+@onready var sprite_3d_2: Sprite3D = $Sprite3D2
 
+		
+		
 func break_self():
 	queue_free()
+	
+func _process(delta):
+	super(delta)
+	sprite_3d.texture = texture
+	sprite_3d_2.texture = texture
